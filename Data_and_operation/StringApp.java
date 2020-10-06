80ce1300c27519460f52e669a1c8005eb698f6aa
gcnml10 <gcnml10@github.com>
2020-10-06 14:45:55 +0900

    Operation and Variable

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,20 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World"); // String
+		//System.out.println('Hello World'); // Character 작은따옴표와 큰 따옴포는 다르다
+		System.out.println("H");
+		
+		System.out.println("Hello "
+				+ "World");
+		
+		// new line
+		System.out.println("Hello \nworld");
+		// escape
+		System.out.println("Hello \"world\""); // Hello "World"
+
+	}
+
+}
