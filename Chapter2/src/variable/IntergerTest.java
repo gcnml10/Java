435c1fb34bfc843a030bdc43747ad1191f81423e
gcnml10 <gskm77037@naver.com>
2020-11-03 22:53:27 +0900

    November Second

---
+++ Chapter2/src/variable/IntergerTest.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class IntergerTest {
+
+	public static void main(String[] args) {
+		
+		byte bs = 127; 
+		
+		System.out.println(bs);
+		
+		//int iVal = 12345678900;
+		long lVal = 12345678900L;
+
+	}
+
+}
